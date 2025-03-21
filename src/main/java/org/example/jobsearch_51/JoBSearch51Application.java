65c394255af75f70871a518c4aa7e6d5d21a1aca
kanat <esenalievkanat.90@gmail.com>
2025-03-21 09:43:49 +0600

    Создан основной класс приложения с точкой входа

---
+++ src/main/java/org/example/jobsearch_51/JoBSearch51Application.java
@@ -0,0 +1,11 @@
+package org.example.jobsearch_51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoBSearch51Application {
+    public static void main(String[] args) {
+        SpringApplication.run(JoBSearch51Application.class, args);
+    }
+}
---
+++ src/test/java/org/example/jobsearch_51/JoBSearch51ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobsearch_51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoBSearch51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
