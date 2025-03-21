5a52ee7ece03a804cd8fe6226b350db9d1ab72ba
kanat <esenalievkanat.90@gmail.com>
2025-03-21 10:50:09 +0600

    Создан интерфейс сервиса для работы с пользователями

---
+++ src/main/java/org/example/jobsearch_51/service/UserService.java
@@ -0,0 +1,14 @@
+package org.example.jobsearch_51.service;
+
+import org.example.jobsearch_51.dto.UserDto;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
+
+public interface UserService {
+    List<UserDto> getAllUsers();
+    UserDto getUserById(int id);
+    void createUser(UserDto userDto);
+    void updateUser(UserDto userDto);
+    String uploadAvatar(MultipartFile file);
+    UserDto getUserByEmail(String email);
+}
