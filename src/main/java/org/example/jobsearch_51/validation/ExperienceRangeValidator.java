03678f9a96700404495834e60f58b3cc3ebc8116
kanat <esenalievkanat.90@gmail.com>
2025-04-01 03:02:44 +0600

    Добавлен валидатор для проверки диапазона опыта работы

--- src/main/java/org/example/jobsearch_51/validation/ExperienceRangeValidator.java
+++ src/main/java/org/example/jobsearch_51/validation/ExperienceRangeValidator.java
@@ -2,15 +2,15 @@ package org.example.jobsearch_51.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.example.jobsearch_51.dto.VacancyDto;
+import org.example.jobsearch_51.dto.WorkExperienceDto;
 
-public class ExperienceRangeValidator implements ConstraintValidator<ValidExperienceRange, VacancyDto> {
+public class ExperienceRangeValidator implements ConstraintValidator<ValidExperienceRange, WorkExperienceDto> {
     @Override
     public void initialize(ValidExperienceRange constraintAnnotation) {
     }
 
     @Override
-    public boolean isValid(VacancyDto vacancy, ConstraintValidatorContext context) {
-        return vacancy.getExpTo() >= vacancy.getExpFrom();
+    public boolean isValid(WorkExperienceDto experience, ConstraintValidatorContext context) {
+        return experience.getExpTo() >= experience.getExpFrom();
     }
 }
